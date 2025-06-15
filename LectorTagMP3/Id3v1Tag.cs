fab5a1b276fc7622a13b10fa9b61be558ffdf880
JoakaG <girbaujoaquina2@gmail.com>
2025-06-14 21:49:19 -0300

    FINALAZACION TP9

---
+++ LectorTagMP3/Id3v1Tag.cs
@@ -0,0 +1,12 @@
+namespace Id3v1Tagespacio
+{
+    public class Id3v1Tag
+    {
+        public string Titulo;
+        public string Artista;
+        public string Album;
+        public string Anio;
+
+    }
+}
+
---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Id3v1Tagespacio;
+
+Console.WriteLine("Ingrese la ruta del archivo MP3:");
+string? path = Console.ReadLine();
+
+if (File.Exists(path))
+{
+    byte[] buffer = new byte[128];
+    using (FileStream MiMP3 = new FileStream(path, FileMode.Open))
+    {
+        MiMP3.Seek(-128, SeekOrigin.End);
+        MiMP3.Read(buffer, 0, 128);
+        var tag = new Id3v1Tag();
+
+        tag.Titulo = System.Text.Encoding.Default.GetString(buffer, 3, 30);
+        tag.Artista = System.Text.Encoding.Default.GetString(buffer, 33, 30);
+        tag.Album = System.Text.Encoding.Default.GetString(buffer, 63, 30);
+        tag.Anio = System.Text.Encoding.Default.GetString(buffer, 93, 4);
+        Console.WriteLine($"Título: {tag.Titulo}");
+        Console.WriteLine($"Artista: {tag.Artista}");
+        Console.WriteLine($"Álbum: {tag.Album}");
+        Console.WriteLine($"Año: {tag.Anio}");
+
+    }
+
+}
+
+else
+{
+    System.Console.WriteLine("Archivo no encontrado");
+}
+
+
